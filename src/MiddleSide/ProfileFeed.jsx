8890c9328341e036edf9f613e438d73b360d01bb
David Glogowski <davidglogowski2003@gmail.com>
2025-01-13 16:20:42 +0100

    temporary fix to user reposted showing on likes tab of user profile even if the user did not repost

--- src/MiddleSide/ProfileFeed.jsx
+++ src/MiddleSide/ProfileFeed.jsx
@@ -161,7 +161,7 @@ function ProfileFeed ({posts, currentUser, setCurrentUser, setPosts}) {
             <div className="flex-[320] text-white flex flex-col-reverse justify-end h-full w-full border-l-2 border-r-2 border-twitterBorder">
                 {userLikedPosts.map((post) => 
                     <div className="w-full h-fit pb-2 border-b-2 border-twitterBorder">
-                        <PostTemplate profileUser={profileUser} currentUser={currentUser} post={post} posts={posts}/>
+                        <PostTemplate currentUser={currentUser} post={post} posts={posts}/>
                     </div>
                 )}
             </div>
